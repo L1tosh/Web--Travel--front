0a60229d8f4f6cf4c9cbb34cbaff2df0fdd04f40
L1tosh <dan.litosh33@gmail.com>
2023-10-04 21:29:48 +0300

    Fixed bug with header/footer

--- js/addTemplate.js
+++ js/addTemplate.js
@@ -1,17 +1,13 @@
-fetch('template/header.html')
+fetch('../templates/header.html')
     .then(response => response.text())
     .then(data => {
-        // Тут ви маєте змогу вставити зміст з header.html на головну сторінку.
-        // Наприклад, можна використовувати innerHTML для вставки в div з певним ідентифікатором.
         document.getElementById('header').innerHTML = data;
     })
     .catch(error => console.error('Помилка завантаження header.html:', error));
 
-fetch('template/footer.html')
+fetch('../templates/footer.html')
     .then(response => response.text())
     .then(data => {
-        // Тут ви маєте змогу вставити зміст з header.html на головну сторінку.
-        // Наприклад, можна використовувати innerHTML для вставки в div з певним ідентифікатором.
         document.getElementById('footer').innerHTML = data;
     })
-    .catch(error => console.error('Помилка завантаження header.html:', error));
+    .catch(error => console.error('Помилка завантаження footer.html:', error));
