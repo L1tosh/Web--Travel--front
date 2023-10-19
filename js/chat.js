5bb4e846978ecdc381ca046776f081b2113431d6
L1tosh <dan.litosh33@gmail.com>
2023-10-19 19:56:07 +0300

    - fixed a bug in the chat scriзpt

--- js/chat.js
+++ js/chat.js
@@ -5,27 +5,25 @@ document.addEventListener('DOMContentLoaded', function () {
     const sendButton1 = document.getElementById('send-button-1');
     const sendButton2 = document.getElementById('send-button-2');
 
-    sendButton1.addEventListener('click', sendMessage);
-    sendButton2.addEventListener('click', sendMessage);
+    sendButton1.addEventListener('click', function () {
+        sendMessage(messageInput1, 'User 1', 'left')
+    });
+    sendButton2.addEventListener('click', function () {
+        sendMessage(messageInput2, 'User 2', 'right')
+    });
 
-    function sendMessage() {
-        const messageUser1 = messageInput1.value.trim();
-        const messageUser2 = messageInput2.value.trim();
-        if (messageUser1 !== '' || messageUser2 !== '') {
+    function sendMessage(massage, user, position) {
+        const massageValue = massage.value.trim();
+
+        if (massageValue !== '') {
             const messageElement = document.createElement('div');
             const userName = document.createElement('div');
             const message = document.createElement('div');
             messageElement.classList.add('message');
 
-            if (messageUser1 !== '') {
-                messageElement.classList.add('left');
-                userName.textContent = "User 1";
-                message.textContent += messageUser1;
-            } else {
-                messageElement.classList.add('right');
-                userName.textContent = "User 2";
-                message.textContent += messageUser2;
-            }
+            messageElement.classList.add(position);
+            userName.textContent = user;
+            message.textContent += massageValue;
 
             messageElement.appendChild(userName);
             messageElement.appendChild(message);
